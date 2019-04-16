e9811c4df5d9717b5eddb65dbfdfafe8da9b2055
Ace Subido <ace.subido@gmail.com>
2019-04-16 19:48:14 +0800

    Add middleman-livereload

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-livereload'
 gem 'sass'
 gem 'slim'
 gem 'tzinfo-data', platforms: %i[mswin mingw jruby]
--- config.rb
+++ config.rb
@@ -13,28 +13,11 @@ set :fonts_dir, 'assets/fonts'
 set :images_dir, 'assets/images'
 set :js_dir, 'assets/javascripts'
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
 
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :development do
+  activate :livereload
+end
